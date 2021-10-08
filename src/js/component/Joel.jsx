b504d497134134eef3c3f0a855cef3a6b22d3d5c
Joel Santiago <jsant288@fiu.edu>
2021-10-08 03:02:47 +0000

    added cards

---
+++ src/js/component/Joel.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+
+//include images into your bundle
+import rigoImage from "../../img/rigo-baby.jpg";
+
+//create your first component
+const Joel = () => {
+	return (
+		<div className="text-center mt-5">
+			<div className="row">
+				<div className="card-deck">
+					<div className="card">
+						<img
+							className="card-img-top"
+							src="..."
+							alt="Card image cap"
+						/>
+						<div className="card-body">
+							<h5 className="card-title">Card title</h5>
+							<p className="card-text">
+								This is a longer card with supporting text below
+								as a natural lead-in to additional content. This
+								content is a little bit longer.
+							</p>
+							<p className="card-text">
+								<small className="text-muted">
+									Last updated 3 mins ago
+								</small>
+							</p>
+						</div>
+					</div>
+					<div className="card">
+						<img
+							className="card-img-top"
+							src="..."
+							alt="Card image cap"
+						/>
+						<div className="card-body">
+							<h5 className="card-title">Card title</h5>
+							<p className="card-text">
+								This card has supporting text below as a natural
+								lead-in to additional content.
+							</p>
+							<p className="card-text">
+								<small className="text-muted">
+									Last updated 3 mins ago
+								</small>
+							</p>
+						</div>
+					</div>
+					<div className="card">
+						<img
+							className="card-img-top"
+							src="..."
+							alt="Card image cap"
+						/>
+						<div className="card-body">
+							<h5 className="card-title">Card title</h5>
+							<p className="card-text">
+								This is a wider card with supporting text below
+								as a natural lead-in to additional content. This
+								card has even longer content than the first to
+								show that equal height action.
+							</p>
+							<p className="card-text">
+								<small className="text-muted">
+									Last updated 3 mins ago
+								</small>
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Joel;
